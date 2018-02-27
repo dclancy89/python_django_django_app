6286a50f4716b140dc8a70e97b0a3a58dbb918ff
Daniel <clancy.daniel@gmail.com>
2018-02-27 15:52:17 -0600

    added the rest of the routes

--- apps/blogs/views.py
+++ apps/blogs/views.py
@@ -8,3 +8,17 @@ def index(request):
 def new(request):
 	response = "Placeholder to display a form to created a new blog"
 	return HttpResponse(response)
+
+def create(request):
+	return redirect('/')
+
+def show(request, number):
+	response = "Placeholder to display blog " + str(number)
+	return HttpResponse(response)
+
+def edit(request, number):
+	response = "placeholder to edit blog " + str(number)
+	return HttpResponse(response)
+
+def destroy(request, number):
+	return redirect('/')
